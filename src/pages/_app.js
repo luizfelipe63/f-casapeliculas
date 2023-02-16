2a00710d45ea0f1d2e84d00e9013d4f6792e7f9b
Felipe <75274860+luizfelipe63@users.noreply.github.com>
2023-02-16 11:23:24 -0300

    Vercel Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,13 @@
 import { GlobalStyle } from '../styles/global'
+import { Analytics } from '@vercel/analytics/react'
 
 GlobalStyle()
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
